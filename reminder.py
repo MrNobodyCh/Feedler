1d2c44c9ea1cec81c63ef8c1d846b6e50c9bef4a
e_chupikov <e_chupikov@wargaming.net>
2017-11-23 12:14:38 +0300

    Changed reminder logic: send notification for all active users.

--- reminder.py
+++ reminder.py
@@ -22,20 +22,24 @@ logging.getLogger("requests").setLevel(logging.WARNING)
 def reminder():
     if requests.get("http://storebot.me").status_code == 200:
         async_bot = telebot.AsyncTeleBot(BotSettings.TOKEN)
-        users = DBGetter(DBSettings.HOST).get("SELECT DISTINCT ON (user_id) * FROM users_subscriptions")
+        users = DBGetter(DBSettings.HOST).get("SELECT user_id FROM users_language WHERE "
+                                              "supported = FALSE AND active_status = TRUE")
         for user in users:
-            not_supported = DBGetter(DBSettings.HOST).get("SELECT user_id FROM users_language "
-                                                          "WHERE user_id = '%s' AND supported = FALSE" % user[0])
-            for x in not_supported:
-                user_id = x[0]
-                markup = types.InlineKeyboardMarkup()
-                markup.add(types.InlineKeyboardButton(text=texts(user_id).RATE_BOT,
-                                                      url='https://telegram.me/storebot?start=Feedler_bot'))
-                markup.add(types.InlineKeyboardButton(texts(user_id).ALREADY_SUPPORTED, callback_data="supported"))
-                msg = async_bot.send_message(user_id, text=texts(user_id).REMINDER,
-                                             reply_markup=markup, parse_mode="Markdown")
-                msg.wait()
-                logging.info("Send reminder for user: %s" % user_id)
+            user_id = user[0]
+            markup = types.InlineKeyboardMarkup()
+            markup.add(types.InlineKeyboardButton(text=texts(user_id).RATE_BOT,
+                                                  url='https://telegram.me/storebot?start=Feedler_bot'))
+            markup.add(types.InlineKeyboardButton(texts(user_id).ALREADY_SUPPORTED, callback_data="supported"))
+            msg = async_bot.send_message(user_id, text=texts(user_id).REMINDER,
+                                         reply_markup=markup, parse_mode="Markdown")
+            result = msg.wait()
+            # check that the user is still active
+            try:
+                logging.info("Send reminder for user: %s" % result.chat.id)
+            except Exception as e:
+                logging.info("%s. No active user with user_id: %s and Response Status: %s" % (e, user_id, result))
+                DBGetter(DBSettings.HOST).insert("UPDATE users_language SET active_status = FALSE "
+                                                 "WHERE user_id = '%s'" % int(user_id))
     else:
         logging.info("Storebot.me is currently unavailable")
 
